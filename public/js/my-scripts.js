f71791d1d2cd5901f36994b33d9488d07c98e333
Diego Gonzalez <gonzalezdiego1990@gmail.com>
2021-02-22 11:27:08 -0300

    Agrega clases desde js
    Cada vez que se presiona el boton + y el boton agregar dentro del modal, se vacian los campos del form

--- public/js/my-scripts.js
+++ public/js/my-scripts.js
@@ -7,9 +7,27 @@ let addButton = document
 
         let title = document.getElementById("title").value;
 
-        let model = `<li
-    class="list-group-item d-flex justify-content-between align-items-center"
-    >${category} | ${title}</li>`;
+        if (title != "") {
+            let model = `<li
+        class="list-group-item d-flex justify-content-between align-items-center"
+        >${category} | ${title}</li>`;
 
-        father.innerHTML += model;
+            father.innerHTML += model;
+
+            document.getElementById("emptyMessage").classList.add("off");
+
+            document.getElementById("my-list").classList.remove("off");
+
+            document.getElementById("title").value = "";
+            document.getElementById("category").value = "🥩 Carnes";
+            document.getElementById("description").value = "";
+        }
+    });
+
+let plusButton = document
+    .getElementById("plus")
+    .addEventListener("click", function () {
+        document.getElementById("title").value = "";
+        document.getElementById("category").value = "🥩 Carnes";
+        document.getElementById("description").value = "";
     });
