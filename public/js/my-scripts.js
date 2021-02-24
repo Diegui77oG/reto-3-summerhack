9c4e06a23444af8c7d94466cc3c800eddbed67ad
Diego Gonzalez <gonzalezdiego1990@gmail.com>
2021-02-24 10:21:56 -0300

    Agrega detalle del producto agregado
    Se cambian los iconos del listado

--- public/js/my-scripts.js
+++ public/js/my-scripts.js
@@ -1,26 +1,29 @@
 let father = document.getElementById("my-container");
 
+let emptyMessage = document.getElementById("emptyMessage");
+
+let myList = document.getElementById("my-list");
+let itemDetail = document.getElementById("item-detail");
+
 let addButton = document
     .getElementById("add")
     .addEventListener("click", function () {
-        let title = document.getElementById("title").value;
+        var cargaProducto = $("#cargaProducto").modal({});
+
+        let product = document.getElementById("product").value;
         let category = document.getElementById("category").value;
         let description = document.getElementById("description").value;
 
-        var cargaProducto = $("#cargaProducto").modal({});
-
-        if (title != "") {
-            let model = `<li
-        class="list-group-item d-flex justify-content-between align-items-center"
-        ><span itemprop="itemListElement">${category} | ${title}</span></li>`;
+        if (product != "" && category != "" && description != "") {
+            let model = `<li class="list-group-item d-flex justify-content-between align-items-center" data-product="${product}" data-product-icon="${category}" data-description="${description}"><span itemprop="itemListElement"><img src="${category}" alt="${product}" class="icono"/> ${product}</span></li>`;
 
             father.innerHTML += model;
 
-            document.getElementById("emptyMessage").classList.add("off");
+            emptyMessage.classList.add("off");
 
-            document.getElementById("my-list").classList.remove("off");
+            myList.classList.remove("off");
 
-            document.getElementById("title").value = "";
+            document.getElementById("product").value = "";
             document.getElementById("category").value = "⭐ Varios";
             document.getElementById("description").value = "";
 
@@ -31,7 +34,30 @@ let addButton = document
 let plusButton = document
     .getElementById("plus")
     .addEventListener("click", function () {
-        document.getElementById("title").value = "";
+        document.getElementById("product").value = "";
         document.getElementById("category").value = "⭐ Varios";
         document.getElementById("description").value = "";
+    });
+
+let myContainer = document
+    .getElementById("my-container")
+    .addEventListener("click", function (e) {
+        myList.classList.add("off");
+        itemDetail.classList.remove("off");
+        document.getElementById("productImage").src = e.target.getAttribute(
+            "data-product-icon"
+        );
+        document.getElementById(
+            "productName"
+        ).innerHTML = e.target.getAttribute("data-product");
+        document.getElementById(
+            "productDescription"
+        ).innerHTML = e.target.getAttribute("data-description");
+    });
+
+let btnBack = document
+    .getElementById("btn-back")
+    .addEventListener("click", function () {
+        myList.classList.remove("off");
+        itemDetail.classList.add("off");
     });
